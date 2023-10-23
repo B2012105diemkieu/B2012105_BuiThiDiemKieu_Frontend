80512cd7826c1e24f6e9c0421852f8b398c29177
B2012105diemkieu <kieuq20021994@gmail.com>
2023-10-23 16:24:40 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//them bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+//them router vao ung dung
+import router from './router';
+createApp(App).use(router).mount("#app");
+
+
--- vite.config.js
+++ vite.config.js
@@ -17,5 +17,13 @@ export default defineConfig({
   //them cau hinh port
   server: {
     port: 3001,
+    //cau hinh proxy (khong gan cung hostname/IP cua API server)
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000/",
+        changeOrigin:true,
+      },
+
+    }
   },
 });
